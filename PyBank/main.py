6b9f3966875e670a4610259b42e697d61328b36e
David Lucey <david.m.lucey@gmail.com>
2021-01-19 22:04:56 -0600

    added dictonary from csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,35 @@
 import os
 import csv
+import math
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
+
+mydict = {}
+
+def Budget(budget_data):
+    # total_profit = sum(budget_data[1])
+    profit_losses = int(budget_data[1])
+
+    print(profit_losses)
+
 
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    budget_dict = {rows[0]:rows[1] for rows in csvreader}
     print(csvreader)
+    print(budget_dict)
 
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    for value  in budget_dict.values():
+        print(value)
+        total_profit = sum(budget_dict.values())
+        print(total_profit)
+    # # Read the header row first 
+    # csv_header = next(csvreader)
+    # print(f"CSV Header: {csv_header}")
+    
+    # Months = len(csvfile.readlines())
+    # print(Months) 
+
+ 
