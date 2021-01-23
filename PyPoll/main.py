898e1865348906e75eb0b612ab579842fe4448c8
David Lucey <david.m.lucey@gmail.com>
2021-01-22 19:41:15 -0600

    have dictonary working with data, still need to process data

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -20,11 +20,12 @@ with open(csvpath) as csvfile:
             candidate_dict[candidate_name] = 1
             # print(csvreader)
 
+        total_votes = sum(candidate_dict.values())
     # Read the header row first (skip this step if there is now header)
     #csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
     print(candidate_dict)
-
+    print(f"Total Votes: {total_votes}")
      
 
     # with open(csvpath) as csvfile:
